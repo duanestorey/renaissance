9af2a2f38fe0a64a73d13d9783f5891449e4943b
Unknown <duanestorey@gmail.com>
2018-12-09 14:42:25 +0000

    Working on adding styles

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Renaissance;
+
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts' );
+add_action( 'after_setup_theme', __NAMESPACE__ . '\\setup_theme' );
+
+function enqueue_scripts() {
+	wp_enqueue_style( 'renaissance', get_template_directory_uri() . '/dist/css/theme.css', array(), '1.0', true );
+}	
+
+function setup_theme() {
+	// Add theme support
+	add_theme_support( 'html5' );
+	add_theme_support( 'title-tag' );
+	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'custom-header' );
+	add_theme_support( 'custom-logo' );
+}
--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 	<head>
-		<title><?php wp_title(); ?></title>
+		<?php wp_head(); ?>
 	</head>
 	<body>
